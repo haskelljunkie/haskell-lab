05ab5cf3b371e1112a2810c43b02713c780f71d6
haskelljunkie <haskelljunkie@gmail.com>
2025-10-23 05:15:24 +0300

    updated Haskell file with new changes

--- training.hs
+++ training.hs
@@ -1,5 +1,15 @@
 nameGreeting' :: String -> String -> String
+
 nameGreeting' hello person 
     | hello == "Hej" = "That is not a real Evi"
     | hello == "Bye" = "that is real Evi"
     | otherwise = hello <> " " <> person
+
+nameGreeting'' :: String -> String -> String
+
+nameGreeting'' greet evi = 
+    greet <> " " <> evi
+    where greet = "Hello"
+          evi = "Evi"
+           
+    
