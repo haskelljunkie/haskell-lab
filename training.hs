1dfc73e3c1641ed4ecd4d6a92e4db6c88d679420
Evgrny <haskelljunkie@gmail.com>
2025-10-22 19:01:53 -0700

    new file added

---
+++ training.hs
@@ -0,0 +1,5 @@
+nameGreeting' :: String -> String -> String
+nameGreeting' hello person 
+    | hello == "Hej" = "That is not a real Evi"
+    | hello == "Bye" = "that is real Evi"
+    | otherwise = hello <> " " <> person
