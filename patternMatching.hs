6f1fbd76e5a5e67e2b6c16cd806adc0b9fb70fb4
haskelljunkie <haskelljunkie@gmail.com>
2025-10-23 05:49:41 +0300

    Pattern file added

---
+++ patternMatching.hs
@@ -0,0 +1,11 @@
+lucky :: Int -> String
+lucky 7 = "Tha's the lucky number"
+lucky x = "Wrong!"
+
+sayMe :: Int -> String
+sayMe 1 = "One"
+sayMe 2 = "Two"
+sayMe 3 = "Three"
+sayMe 4 = "Four"
+sayMe 5 = "Five"
+sayMe n = "Not Between one and five"
