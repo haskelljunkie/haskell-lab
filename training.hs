fa83f9cd4825b749ae7b29d58875307c17d30c2c
haskelljunkie <haskelljunkie@gmail.com>
2025-10-24 03:06:24 +0300

    trainingTODO8910

--- training.hs
+++ training.hs
@@ -1,3 +1,5 @@
+
+
 nameGreeting' :: String -> String -> String
 
 nameGreeting' hello person 
@@ -13,4 +15,42 @@ nameGreeting'' greet evi =
           evi = "Evi"
         
 
-    
+bmiCalc ::Double -> Double -> String
+bmiCalc height weight 
+    | bmi <= 18.5 = "Underweight"
+    | bmi > 18.5 && bmi <= 23 = "Normal"
+    | otherwise = "Overweight"    
+    where bmi = weight / height^2
+
+max' :: (Ord a) => a -> a -> a
+max' a b 
+    | a >= b = a
+    | otherwise = b
+
+
+myAbs :: Int -> Int
+myAbs a
+    | a < 0 = -a
+    | otherwise = a
+
+describe :: Int -> String
+
+describe n 
+    | n == 1 = "One"
+    | n == 2 = "Two"
+    | otherwise = "Many"
+
+safeHead :: [a] -> String
+safeHead [] = "empty"
+safeHead (_:_) = "not empty"
+
+--Напиши функция Sum of list elements ,Използвай pattern matching
+mySum :: [Int] -> Int
+mySum [] = 0
+mySum (x:xs) = x + mySum xs
+
+initials :: String -> String -> String
+initials (x:_) (y:_) = [x] ++". " ++ [y] ++"."
+-- за да превърнеш Char в String, можеш да използваш [x], т.е. списък от един символ.
+
+--TODO: task number 8,9 and 10 
