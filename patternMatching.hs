995f353017156d3973b684da1ec0ae0aa72b88c2
haskelljunkie <haskelljunkie@gmail.com>
2025-10-30 07:39:04 +0200

    More patternMatching.hs

--- patternMatching.hs
+++ patternMatching.hs
@@ -9,3 +9,34 @@ sayMe 3 = "Three"
 sayMe 4 = "Four"
 sayMe 5 = "Five"
 sayMe n = "Not Between one and five"
+
+--Задачи Pattern patching 
+
+isEmpty :: [a] -> Bool 
+isEmpty [] = True
+isEmpty _ = False 
+
+firstElement :: [a] -> String
+firstElement [] = "No elements"
+firstElement [_] = "One element"
+firstElement (_:_:_) = "More than one"
+
+sumPair :: (Int, Int) -> Int
+sumPair (x, y) = x + y
+
+myLength :: [a] -> Int 
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
+
+
+sumList :: [Int] -> Int
+sumList [] = 0
+sumList (x:xs) = x + sumList xs 
+
+describeTuple :: (Int, Int, Int) -> String
+
+describeTuple (x, y, z)
+    | x == y &&  y == z = "All equal"
+    | x == y            = "First two equal"
+    | otherwise         = "All different"
+
