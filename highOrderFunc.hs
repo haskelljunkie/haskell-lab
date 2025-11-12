cc3d25d180209657c1cf1fc95cdaddd0ba509815
haskelljunkie <haskelljunkie@gmail.com>
2025-11-12 11:56:04 +0200

    map and filter|

--- highOrderFunc.hs
+++ highOrderFunc.hs
@@ -3,3 +3,27 @@ zipWith'_ :: [a1] -> p -> [a2]
 zipWith'_ [] _  = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys 
+
+--maps and filters
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs 
+
+squares :: Num a => [a] -> [a]
+squares = map(^2) 
+
+squares' :: Num a => [a] -> [a]
+squares' [] = []
+squares' (x:xs) = (x^2) : squares' xs 
+
+-- Използвай map, за да добавиш 10 към всеки елемент от [1..5]
+
+plusTen :: Num a => [a] -> [a]
+plusTen = map (+10)
+
+--TODO:Направи функция, която взима само четните числа от списък
+
+--takeEven :: Num a => [a] -> [a]
+--takeEven [] = []
+--takeEven (x:xs) = map (x `mod` 2 == 0) : 
