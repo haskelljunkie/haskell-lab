87d69640503636216abad8d0919062ffa5a32d91
haskelljunkie <haskelljunkie359@proton.me>
2025-11-12 17:39:58 +0200

    TODO:Done

--- highOrderFunc.hs
+++ highOrderFunc.hs
@@ -32,15 +32,42 @@ evenFilter = filter (even) [1..20]
 --endsWithS :: [String] ->[String]
 --endsWithS :: [Char] -> Bool
 endsWithS :: [String] -> Bool
-endsWithS = (\word -> last word == "s")
+endsWithS xs = (\word -> last word == "s") xs
 
 
 
 
 
 
---TODO:Направи функция, която взима само четните числа от списък
+--Направи функция, която взима само четните числа от списък
+takeEven :: [Integer] -> [Integer]
+takeEven = filter even
 
---takeEven :: Num a => [a] -> [a]
---takeEven [] = []
---takeEven (x:xs) = map (x `mod` 2 == 0) : 
+--Искаш ли да направим и takeOdd по същия начин, за упражнение?
+
+takeOdd :: [Integer] -> [Integer]
+takeOdd = filter odd
+
+--Направи функция addOneToAll, 
+--която взима списък от числа и връща нов списък, в който всеки елемент е увеличен с 1.
+--Напиши го първо с lambda, после можем да го превърнем в pointfree стил.
+
+addOneNew :: Num a => [a] -> [a]
+addOneNew = map (\x -> x +1 ) 
+
+addOneNew' :: Num a => [a] -> [a]
+addOneNew' = map (+1)
+
+{-Направи функция squareEvenNew, която:
+  - Взима списък от числа.
+  - Избира само четните числа (filter).
+  - Връща нов списък, в който тези числа са на квадрат (map).
+  - Можеш първо да го напишеш с lambda за map и filter, после можем да го превърнем в pointfree стил.-}
+
+squareEvenNew' :: [Integer] -> [Integer]
+squareEvenNew' = map(\x ->x^2) . filter even
+
+squareEvenNew :: [Integer] -> [Integer]
+squareEvenNew = map(^2).filter even
+
+ 
