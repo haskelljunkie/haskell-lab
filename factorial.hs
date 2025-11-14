efbedadefe25c1fa02f00d8678d830ef1ae85cf1
Евгени Стоянов Екимов <haskelljunkie359@proton.me>
2025-11-15 01:29:44 +0200

    Update factorial.hs

--- factorial.hs
+++ factorial.hs
@@ -9,8 +9,5 @@ testvai  = factorial 5
 factorial' :: Integer -> Integer
 factorial' n = product [1..n]
 
-fibonachi :: Int -> Int 
-fibonachi 0 = 0
-fibonachi 1 = 1
-fibonachi n = (n-1) + (n-2)
+
  
