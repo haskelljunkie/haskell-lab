ee2cdd988d2ab0b9777261537715f5ac5205b610
haskelljunkie <haskelljunkie359@proton.me>
2025-11-19 18:53:30 +0200

    pointfree tasks

---
+++ pointfree.hs
@@ -0,0 +1,22 @@
+{-Задача 1:
+Напиши функция, която взема списък от числа и връща нов списък, в който всяко число е увеличено с 1.
+Но — ще стигнем до pointfree версията ѝ постепенно, не изведнъж.-}
+
+numLists :: Num a => [a] -> [a]
+numLists [] = [] 
+numLists (x:xs) = (x + 1) : numLists xs  
+
+numLists' ::  Num a => [a] -> [a]
+numLists'  =  map (+1)   
+
+numLists'' :: (Num a, Enum a) => [a] -> [a]
+numLists''  = map succ   
+
+{-Задача 2:
+Направи функция squareEven, която: Взима списък от числа. Избира само четните числа (filter).
+Връща нов списък, в който тези числа са на квадрат (map).
+Можеш първо да го напишеш с lambda за map и filter, после можем да го превърнем в pointfree стил.-}
+
+squareEven  = map(^2) . filter even  
+
+squareEven'  = map(\x -> x^2) . filter even
