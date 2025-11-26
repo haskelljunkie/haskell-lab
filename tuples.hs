48a34f07f50963ec4d27224af9381fbbdd5bddb5
haskelljunkie <haskelljunkie@proton.me>
2025-11-26 16:28:21 +0200

    todo

--- tuples.hs
+++ tuples.hs
@@ -2,63 +2,63 @@ module TupleTasks where
 
 -- 1. Създай tuple с две числа
 pair1 :: (Int, Int)
-pair1 = undefined
+pair1 = (2,3)
 
 -- 2. Създай tuple със стринг и число
 pair2 :: (String, Int)
-pair2 = undefined
+pair2 = ("Haskell", 4)
 
 -- 3. Създай tuple с три елемента (име, възраст, град)
 person :: (String, Int, String)
-person = undefined
+person = ("Evi",44,"Bucharest")
 
 -- 4. Напиши функция, която връща първия елемент на tuple (a,b)
 firstOf :: (a, b) -> a
-firstOf = undefined
+firstOf = fst
 
 -- 5. Напиши функция, която връща втория елемент на tuple (a,b)
 secondOf :: (a, b) -> b
-secondOf = undefined
+secondOf = snd
 
 -- 6. Напиши функция, която събира двете числа от tuple (x,y)
 sumPair :: Num a => (a, a) -> a
-sumPair = undefined
+sumPair (x,y)= (+) x y
 
 -- 7. Функция, която умножава числата от triple (x,y,z)
 prodTriple :: Num a => (a, a, a) -> a
-prodTriple = undefined
+prodTriple (x,y,z) =  x * y * z
 
 -- 8. Размени елементите на tuple (a,b) -> (b,a)
 swap :: (a, b) -> (b, a)
-swap = undefined
+swap (a,b) = (b,a) 
 
 -- 9. Функция, която взема (име, възраст) и връща описание като String
 describe :: (String, Int) -> String
-describe = undefined
+describe (name,age) = name ++ " " ++ show age 
 
 -- 10. Функция, която връща True, ако поне един Bool в tuple е True
 eitherTrue :: (Bool, Bool) -> Bool
-eitherTrue = undefined
+eitherTrue (x,y)= x || y 
 
 -- 11. Събери два tuple-а координати: (a,b) и (c,d) -> (a+c, b+d)
 addPairs :: Num a => (a, a) -> (a, a) -> (a, a)
-addPairs = undefined
+addPairs (a,b) (c,d) = (a+c, b+d) 
 
 -- 12. Вземи списък от tuple-и и върни списък от първите елементи
 firsts :: [(a, b)] -> [a]
-firsts = undefined
+firsts = map fst 
 
 -- 13. Вземи списък от tuple-и и върни списък от вторите елементи
 seconds :: [(a, b)] -> [b]
-seconds = undefined
+seconds  = map snd  
 
 -- 14. Вземи три triple-а и върни tuple от първите им елементи
 tripleFirsts :: ((a,b,c), (a,b,c), (a,b,c)) -> (a,a,a)
-tripleFirsts = undefined
+tripleFirsts = --TODO
 
 -- 15. Функция, която повдига квадрат всяко число в tuple (x,y)
-squarePair :: Num a => (a, a) -> (a, a)
-squarePair = undefined
+--squarePair :: Num a => (a, a) -> (a, a)
+--squarePair = undefined
 
 -- 16. Приеми списък от (име, възраст) и върни имената на пълнолетните
 adults :: [(String, Int)] -> [String]
